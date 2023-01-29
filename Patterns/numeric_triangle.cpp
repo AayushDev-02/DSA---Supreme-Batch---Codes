58ae59a882afb1073ddf609de57768fbd9844b88
Aayush Yadav <yadavaayush002@gmail.com>
2023-01-29 14:19:29 +0530

    29/01/23

---
+++ Patterns/numeric_triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    cout << endl;
+
+    for(int i=0; i<n; i++){
+        for(int j=1; j<= i+1; j++){
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+
+    return 0;
+}
