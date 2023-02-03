76b488db48dbadf66ad2172329d7895fbebc178b
Aayush Yadav <yadavaayush002@gmail.com>
2023-02-03 23:03:42 +0530

    02/02/23

--- numeric_pelindrome_pyramid.cpp
+++ numeric_pelindrome_pyramid.cpp
@@ -15,5 +15,25 @@ int main(){
 
         cout << endl;
     }
+    
+    cout << endl;
+
+    for(int i=0; i<5; i++){
+        int j=0;
+        for(; j<i+1; j++){
+            int ans = j + 1;
+            char ch= ans + 'A' - 1;
+            cout << ch <<  " ";
+        }
+        j=j-1;
+        for(; j>0; j--){
+            int ans = j;
+            char ch = ans+ 'A' - 1;
+            cout << ch << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
     return 0;
 }
