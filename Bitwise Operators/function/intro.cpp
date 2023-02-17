ce9e2fdbf968698f00ca6d21f133146ba79453dc
Aayush Yadav <yadavaayush002@gmail.com>
2023-02-17 21:03:15 +0530

    added patterns

---
+++ Bitwise Operators/function/intro.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "AayusH Yadav" << endl;
+    return 0;
+}
---
+++ Bitwise Operators/variable_scoping.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    //declaration
+    int a;
+    // initialization
+    int b=5;
+    //updation
+    b=10;
+
+    // cannot redefine 
+    // int b= 6;       // error
+
+    
+    return 0;
+}
