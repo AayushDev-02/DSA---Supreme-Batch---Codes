763ed44a67ec5e4e5f1c1037e632b347beade2c5
Aayush Yadav <yadavaayush002@gmail.com>
2023-05-13 13:33:54 +0530

    Trees

---
+++ Tree/LeftView.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public: 
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data){
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+void leftVeiw(Node* root, int *level, vector<int> *arr){
+    if(root == NULL){
+        return;
+    }
+
+    if(level == arr.size()){
+        arr.push_back(root->data);
+    }
+
+    root->left = leftView(root->left, level+1, arr);
+    root->right = leftView(root->right, level+1, arr);
+}
+
+
+int main(){
+
+}
