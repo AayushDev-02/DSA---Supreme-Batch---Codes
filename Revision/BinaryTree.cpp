ac4afe6ae501706871b5f390be9709a732ec7b6d
Aayush Yadav <yadavaayush002@gmail.com>
2023-06-07 14:34:42 +0530

    revision

--- Revision/BinaryTree.cpp
+++ Revision/BinaryTree.cpp
@@ -421,7 +421,7 @@ vector<int> verticalTraversal(Node* root){
     }
 
     map<int, map<int, vector<int>>> nodes;
-    queue<Node*, pair<int,int>> q;
+    queue<pair<Node*, pair<int,int>>> q;
     q.push(root, make_pair(0,0));
 
     while(!q.empty()){
@@ -455,7 +455,85 @@ vector<int> verticalTraversal(Node* root){
     return ans;
 }
 
+//top view
+vector<int> topView(Node* temp){
+    
+    vector<int> ans;
+    if(root == NULL){
+        return ans;
+    }
 
+    map<int,int> nodes;
+    queue<pair<Node* , int>> q;
+
+    q.push(make_pair(root, 0));
+
+    while(!q.empty()){
+        pair<Node*, int> temp = q.front();
+        q.pop();
+        Node* frontNode = temp.first;
+        int hd = temp.second;
+
+        //inserting into map
+
+        if(nodes.find(hd) == nodes.end()){
+            nodes[hd] = frontNode->data;
+        }
+        
+        if(frontNode->left) {
+            q.push(make_pair(frontNode->left, hd-1));
+        }
+
+        if(frontNode->right){
+            q.push(make_pair(frontNode->right, hd+1));
+        }
+    }
+
+    for(auto i: nodes){
+        ans.push_back(i.second);
+    }
+
+    return ans;
+}
+
+// Bottom View 
+
+vector<int> bottomView(Node* root){
+
+    vector<int> ans;
+
+    if(root == NULL){
+        return ans;
+    }
+
+    map<int,int> nodes;
+    queue<pair<Node*, int>> q;
+    q.push(make_pair(root, 0));
+
+    while(!q.empty()){
+        pair<Node*, int> temp = q.front();
+        q.pop();
+        Node* frontNode = temp.first;
+        int hd = temp.second;
+
+        //inserting into the map
+        nodes[hd] = frontNode->data;
+
+        if(frontNode->left){
+            q.push(make_pair(frontNode->left, hd-1));
+        }
+
+        if(frontNode->right){
+            q.push(make_pair(frontNode->right, hd+1));
+        }
+    }
+
+    for(auto i: nodes){
+        ans.push_back(i.second);
+    }
+
+    return ans;
+}
 
 int main(){
 
