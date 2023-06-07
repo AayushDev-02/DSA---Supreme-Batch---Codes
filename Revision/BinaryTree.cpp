da8d139e0f8d6c615652a9e9d576a21f71a38841
Aayush Yadav <yadavaayush002@gmail.com>
2023-06-07 08:44:43 +0530

    revision

--- Revision/BinaryTree.cpp
+++ Revision/BinaryTree.cpp
@@ -266,6 +266,33 @@ bool checkIdentical(Node* root1, Node* root2){
     }
 
     return left && right && cnd3;
+}
+
+//sum tree check
+pair<int, bool> checkSumTree(Node* root){
+    if(root == NULL){
+        pair<int,bool> p = make_pair(0,true);
+        return p;
+    }
+
+    if(root->left == NULL && root->right == NULL){
+        pair<int,bool> p = make_pair(root->data, true);
+        return p;
+    }
+
+    pair<int,bool> left = checkSumTree(root->left);
+    pair<int,bool> right = checkSumTree(root->right);
+
+    bool cnd1 = left.second;
+    bool cnd2 = right.second;
+
+    bool cnd3 = (left.first + right.first == root->data);
+
+    pair<int, bool> ans;
+    ans.first = left.first + right.first + root->data;
+    ans.second = cnd1&&cnd2&&cnd3;
+
+    return ans;
 
 }
 
